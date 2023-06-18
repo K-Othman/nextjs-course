d022c5ffcbe6b1f10e44931ece8b023bf4c44ff0
K-Othman <karim_othman88@hotmail.com>
2023-06-18 17:27:54 +0100

    learning a different way of setting link hrefs

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,7 +4,21 @@ function ClientProjectPage() {
   const router = useRouter();
 
   console.log(router.query);
-  return <div>The Projects of a given client</div>;
+
+  function loadProjectHandler() {
+    // router.push("/clients/karim/projecta");
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "karim", clientprojectid: "projecta" },
+    });
+  }
+
+  return (
+    <div>
+      <h1>The Projects of a given client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
 }
 
 export default ClientProjectPage;
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,28 @@
+import Link from "next/dist/client/link";
+
 function ClientPage() {
+  const clients = [
+    { id: "karim", name: "Karim" },
+    { id: "memo", name: "Memo" },
+  ];
+
   return (
     <div>
       <h1>The Client Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
